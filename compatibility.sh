576505557fdc0e757d3c5c050462c72786af536a
Robert de Bock <robert@meinit.nl>
2018-04-01 07:00:26 +0200

    Reordering and using failable ci/cd. Remove bug from comp. script.

--- compatibility.sh
+++ compatibility.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 
 echo "Compatibility"
 echo "-------------"
@@ -7,11 +7,11 @@ echo "This role has been tested against the following distributions and Ansible 
 echo ""
 echo "|distribution|ansible 2.3|ansible 2.4|ansible 2.5|"
 echo "|------------|-----------|-----------|-----------|"
-cat .travis.yml | docker run -i --rm jlordiales/jyparser get ".env" | while read dash version distro ; do
+cat .travis.yml | docker run -i --rm jlordiales/jyparser get ".env" | while read dash version distro rest ; do
   echo "${distro}" | cut -d\" -f2 | sort | uniq | while read distribution ; do
-  grep "${distribution}" .travis.yml | grep 2.4 > /dev/null && v24=yes || v24=no
-  grep "${distribution}" .travis.yml | grep 2.5 > /dev/null && v25=yes || v25=no
-  grep "${distribution}" .travis.yml | grep 2.6 > /dev/null && v26=yes || v26=no
-  echo "|${distribution}|${v24}|${v25}|${v26}|"
+  grep "${distribution}" .travis.yml | grep -v 'fail' | grep '>=2.3,<2.4' > /dev/null && v23=yes || v23=no
+  grep "${distribution}" .travis.yml | grep -v 'fail' | grep '>=2.4,<2.5' > /dev/null && v24=yes || v24=no
+  grep "${distribution}" .travis.yml | grep -v 'fail' | grep '>=2.5,<2.6' > /dev/null && v25=yes || v25=no
+  echo "|${distribution}|${v23}|${v24}|${v25}|"
  done
 done | sort | uniq
